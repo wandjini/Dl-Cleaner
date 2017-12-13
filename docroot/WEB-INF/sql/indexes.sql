a72a82772fe5c119dbea92759e6ecacd1a41fd7e
Guy Wandji <guy.wandji@prjteam.com>
2017-12-13 09:45:46 +0100

    Update ORM

--- docroot/WEB-INF/sql/indexes.sql
+++ docroot/WEB-INF/sql/indexes.sql
@@ -3,5 +3,15 @@ create index IX_EA70AEE6 on DlCleaner_FileToClean (groupId, deleted);
 create index IX_22FBBC76 on DlCleaner_LostFile (groupId, deleted);
 create unique index IX_D5A61C91 on DlCleaner_LostFile (groupId, fileEntryId, dlFileVersionId);
 
+create index IX_60F5A4EA on DlCleaner_UnusedFile (companyId, deleted);
 create index IX_E9D9D4E8 on DlCleaner_UnusedFile (groupId, deleted);
 create unique index IX_16941F5F on DlCleaner_UnusedFile (groupId, fileEntryId, dlFileVersionId);
+
+create index IX_D37A8D23 on DlCleaner_WcOrphanFile (groupId);
+create unique index IX_683ABB76 on DlCleaner_WcOrphanFile (groupId, dlFileUuId);
+
+create unique index IX_F8B061E3 on DlCleaner_WcReferencedFile (companyId, dlFileUuId);
+create index IX_B3C8210C on DlCleaner_WcReferencedFile (companyId, orphan);
+create index IX_73D5B992 on DlCleaner_WcReferencedFile (groupId);
+create unique index IX_4803B965 on DlCleaner_WcReferencedFile (groupId, dlFileUuId);
+create unique index IX_3E41959C on DlCleaner_WcReferencedFile (groupId, dlFileUuId, articleId);
--- docroot/WEB-INF/sql/tables.sql
+++ docroot/WEB-INF/sql/tables.sql
@@ -37,6 +37,35 @@ create table DlCleaner_UnusedFile (
 	fileEntryId LONG,
 	dlFileVersionId LONG,
 	dlFileTitle VARCHAR(75) null,
+	dlFileUuId VARCHAR(75) null,
 	deleted BOOLEAN,
 	comment_ STRING null
+);
+
+create table DlCleaner_WcOrphanFile (
+	wcOrphanFileId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	articleId VARCHAR(75) null,
+	dlFileUuId VARCHAR(75) null,
+	cleaned BOOLEAN,
+	type_ VARCHAR(75) null
+);
+
+create table DlCleaner_WcReferencedFile (
+	wcRefencedFileId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	wcUrlTitle VARCHAR(75) null,
+	dlFileUuId VARCHAR(75) null,
+	orphan BOOLEAN,
+	type_ VARCHAR(75) null
 );
